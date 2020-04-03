5ab839d81d8d6c4652915c0af20abaa8b5d72fa2
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-04-03 23:04:52 +0530

    Updated config and AndroidManifest

---
+++ lib/input.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    color: Color(0xff10ac84),
+                    borderRadius: BorderRadius.circular(10.0)),
+        ),
+          ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    color: Color(0xff10ac84),
+                    borderRadius: BorderRadius.circular(10.0)),
+        ),
+          ),
+              ],),),
+          Expanded(
+            child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    color: Color(0xff10ac84),
+                    borderRadius: BorderRadius.circular(10.0)),
+        ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    color: Color(0xff10ac84),
+                    borderRadius: BorderRadius.circular(10.0)),
+        ),
+          ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                    color: Color(0xff10ac84),
+                    borderRadius: BorderRadius.circular(10.0)),
+        ),
+          ),
+              ],),),
+        ],)
+    );
+  }
+}
+
+      
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -8,31 +9,12 @@ class BMICalculator extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        primaryColor: Color(0xff10ac84),
+        scaffoldBackgroundColor: Color(0xff535c68),
         accentColor: Colors.purple,
         textTheme: TextTheme(body1:TextStyle(color: Colors.white))
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('BMI CALCULATOR')),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
     );
   }
 }
